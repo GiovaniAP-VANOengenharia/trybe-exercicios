7fefab77c5b8c43e99e7b238e034ed49051c2060
vanoengenharia <vanoengenharia@protonmail.com>
2022-05-14 17:13:20 -0300

    Add exercícios dia 4.2

--- fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
@@ -1,5 +1,13 @@
 let numbers = [5,9,3,19,70,8,100,2,35,27];
 
+/// exercicio 01 ///
+
+for (i of numbers){
+    console.log(i);
+}
+
+/// exercicio 01 ///
+
 for (i of numbers){
     console.log(i);
 }
